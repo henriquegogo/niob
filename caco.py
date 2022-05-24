5bb91eb9fd33902d2b0d42c2c07c1dac231271f1
Henrique Gogó <henriquegogo@gmail.com>
2022-05-24 01:29:06 -0300

    Env vars and builtin commands

--- caco.py
+++ caco.py
@@ -1,5 +1,19 @@
-STRING, TEXT, EOL = 'STRING', 'TEXT', 'EOL'
-text: str = ''
+IDENTIFIER, STRING, EOL = 'IDENTIFIER', 'STRING', 'EOL'
+
+def _set(name, value):
+    env['vars'][name] = value;
+
+def _print(arg):
+    value = arg if is_quote(arg[0]) else env['vars'][arg]
+    print(value)
+
+env = {
+    'vars': {},
+    'cmds': {
+        'set': _set,
+        'print': _print
+    }
+}
 
 class Token():
     def __init__(self, type: str, pos: int = 0, length: int = 0):
@@ -8,10 +22,20 @@ class Token():
         self.length = length
 
 # PARSER
-def parser(tokens):
+def parser(tokens: list[Token], text: str):
+    cmd, args = '', []
+
     for token in tokens:
         value = text[token.pos : token.pos + token.length]
         print("> ", token.type, value)
+
+        if token.type == EOL:
+            env['cmds'][cmd](*args)
+            cmd, args = '', []
+        elif token.type == IDENTIFIER and not cmd:
+            cmd = value
+        else:
+            args.append(value)
 
 # LEXER
 def is_eol(ch: str) -> bool:
@@ -23,40 +47,39 @@ def is_char(ch: str) -> bool:
 def is_quote(ch: str) -> bool:
     return ch == '"' or ch == '\''
 
-def lexer() -> list[Token]:
+def lexer(text: str) -> list[Token]:
     tokens: list[Token] = []
-    text_pos: int = 0
+    pos: int = 0
 
-    while text_pos < len(text):
-        while not is_char(text[text_pos]): text_pos += 1
-        initial_pos: int = text_pos
+    while pos + 1 < len(text):
+        while not is_char(text[pos]): pos += 1
+        init_pos: int = pos
 
-        if is_quote(text[text_pos]):
-            quote_char: str = text[text_pos]
-            text_pos += 1
-            while text[text_pos] != quote_char: text_pos += 1
-            tokens.append(Token(TEXT, initial_pos, text_pos - initial_pos + 1))
+        if is_quote(text[pos]):
+            quote_char: str = text[pos]
+            pos += 1
+            while text[pos] != quote_char: pos += 1
+            tokens.append(Token(STRING, init_pos, pos - init_pos + 1))
         else:
-            while is_char(text[text_pos]): text_pos += 1
-            tokens.append(Token(STRING, initial_pos, text_pos - initial_pos))
+            while is_char(text[pos]): pos += 1
+            tokens.append(Token(IDENTIFIER, init_pos, pos - init_pos))
 
-        if is_eol(text[text_pos]) or is_eol(text[text_pos + 1]):
+        if is_eol(text[pos]) or is_eol(text[pos + 1]):
             tokens.append(Token(EOL))
 
-        text_pos += 1
+        pos += 1
 
     return tokens
 
 def read_file(filename: str):
-    global text
     file = open(filename, 'r')
     lines: list = file.readlines()
-    text = "".join(lines)
+    return "".join(lines)
 
 def main():
-    read_file("script.caco");
-    tokens = lexer();
-    parser(tokens);
+    text = read_file("script.caco")
+    tokens = lexer(text)
+    parser(tokens, text)
 
 if __name__ == '__main__':
     main()

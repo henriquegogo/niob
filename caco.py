b515ada558780b0725584e8c5accc787a7609dae
Henrique Gogó <henriquegogo@gmail.com>
2022-06-02 02:19:39 -0300

    Using nested commands

--- caco.py
+++ caco.py
@@ -12,6 +12,9 @@ class Environment():
     def func(self, key, cmd):
         setattr(self, key, cmd)
 
+    def sum(self, a, b):
+        return float(a) + float(b)
+
 class Token():
     def __init__(self, type: str, pos: int = 0, length: int = 0):
         self.type = type
@@ -23,17 +26,27 @@ def parser(tokens: list, text: str):
     env = Environment()
     env.func('puts', print)
 
-    cmd, args = '', []
+    cmd, cmd_args = '', []
+    nested_cmd, args = '', []
 
     for token in tokens:
         value = text[token.pos : token.pos + token.length]
         print("> ", token.type, value)
 
         if token.type == EOL:
+            if nested_cmd:
+                cmd_return = getattr(env, nested_cmd)(*args)
+                cmd_args.append(cmd_return)
+                args = cmd_args
             getattr(env, cmd)(*args)
-            cmd, args = '', []
+            cmd, cmd_args = '', []
+            nested_cmd, args = '', []
         elif token.type == IDENTIFIER and hasattr(env, value) and not cmd:
             cmd = value
+        elif token.type == IDENTIFIER and hasattr(env, value) and cmd:
+            cmd_args = args.copy()
+            args = []
+            nested_cmd = value
         elif token.type == VARIABLE:
             args.append(getattr(env.vars, value))
         else:
@@ -46,12 +59,6 @@ def is_eol(ch: str) -> bool:
 def is_char(ch: str) -> bool:
     return ch != '\t' and ch != ' ' and not is_eol(ch)
 
-def is_quote(ch: str) -> bool:
-    return ch == '"' or ch == '\''
-
-def is_var_prefix(ch: str) -> bool:
-    return ch == '$'
-
 def lexer(text: str) -> list:
     tokens: list = []
     pos: int = 0
@@ -60,12 +67,12 @@ def lexer(text: str) -> list:
         while not is_char(text[pos]): pos += 1
         init_pos: int = pos
 
-        if is_quote(text[pos]):
+        if text[pos] == '"' or text[pos] == '\'':
             quote_char: str = text[pos]
             pos += 1
             while text[pos] != quote_char: pos += 1
             tokens.append(Token(STRING, init_pos + 1, pos - init_pos - 1))
-        elif is_var_prefix(text[pos]):
+        elif text[pos] == '$':
             while is_char(text[pos]): pos += 1
             tokens.append(Token(VARIABLE, init_pos + 1, pos - init_pos - 1))
         else:

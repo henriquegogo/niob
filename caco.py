dd8ec2c1fe957ea9cf38b93b79d0dd0bcff3c599
Henrique Gogó <henriquegogo@gmail.com>
2022-06-03 02:59:49 -0300

    Variables as Linked List too. Now keys can be anything

--- caco.py
+++ caco.py
@@ -1,19 +1,32 @@
 IDENTIFIER, STRING, VARIABLE, EOL = 'IDENTIFIER', 'STRING', 'VARIABLE', 'EOL'
 
 class Environment():
-    class Variables(): pass
-
-    def __init__(self):
-        self.vars = self.Variables()
-
-    def set(self, key, value):
-        setattr(self.vars, key, value)
-
     def func(self, key, cmd):
         setattr(self, key, cmd)
 
     def sum(self, a, b):
         return float(a) + float(b)
+
+class Variable():
+    def __init__(self, key: str = '', value: str = ''):
+        self.key = key
+        self.value = value
+        self.next = None
+
+    def save(self, key: str, value: str):
+        current: Variable = self
+        while current.next != None:
+            current = current.next
+            if current.key == key:
+                current.value = value
+                return
+        current.next = Variable(key, value)
+
+    def find(self, key: str):
+        current: Variable = self
+        while current.next != None:
+            current = current.next
+            if current.key == key: return current.value
 
 class Token():
     def __init__(self, type: str = '', pos: int = 0, length: int = 0):
@@ -22,15 +35,20 @@ class Token():
         self.length = length
         self.next = None
 
-    def add_last(self, last):
+    def add(self, last):
         current: Token = self
         while current.next != None: current = current.next
-        setattr(current, 'next', last)
+        current.next = last
 
 # PARSER
 def parser(token: Token, text: str):
+    vars = Variable()
+
     env = Environment()
+    env.func('set', vars.save)
+    env.func('=', vars.save)
     env.func('puts', print)
+    env.func('+', env.sum)
 
     cmds, cmds_args, args = [], [], []
 
@@ -56,7 +74,7 @@ def parser(token: Token, text: str):
                 args = []
             cmds.insert(0, value)
         elif token.type == VARIABLE:
-            args.append(getattr(env.vars, value))
+            args.append(vars.find(value))
         else:
             args.append(value)
 
@@ -69,7 +87,7 @@ def is_char(ch: str) -> bool:
 
 def lexer(text: str) -> Token:
     pos: int = 0
-    head_token: Token = Token()
+    tokens: Token = Token()
 
     while pos + 1 < len(text):
         while not is_char(text[pos]): pos += 1
@@ -79,20 +97,20 @@ def lexer(text: str) -> Token:
             quote_char: str = text[pos]
             pos += 1
             while text[pos] != quote_char: pos += 1
-            head_token.add_last(Token(STRING, init_pos + 1, pos - init_pos - 1))
+            tokens.add(Token(STRING, init_pos + 1, pos - init_pos - 1))
         elif text[pos] == '$':
             while is_char(text[pos]): pos += 1
-            head_token.add_last(Token(VARIABLE, init_pos + 1, pos - init_pos - 1))
+            tokens.add(Token(VARIABLE, init_pos + 1, pos - init_pos - 1))
         else:
             while is_char(text[pos]): pos += 1
-            head_token.add_last(Token(IDENTIFIER, init_pos, pos - init_pos))
+            tokens.add(Token(IDENTIFIER, init_pos, pos - init_pos))
 
         if is_eol(text[pos]) or is_eol(text[pos + 1]):
-            head_token.add_last(Token(EOL))
+            tokens.add(Token(EOL))
 
         pos += 1
 
-    return head_token
+    return tokens
 
 def read_file(filename: str):
     file = open(filename, 'r')
@@ -101,8 +119,8 @@ def read_file(filename: str):
 
 def main():
     text = read_file("script.caco")
-    head_token = lexer(text)
-    parser(head_token, text)
+    tokens = lexer(text)
+    parser(tokens, text)
 
 if __name__ == '__main__':
     main()
